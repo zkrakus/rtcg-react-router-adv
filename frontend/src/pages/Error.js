4bea3e2c535a9d50cd2634dad644f5f50efc81e0
zkrakus <zkrakus@gmail.com>
2025-06-18 12:51:43 -0400

    feat:s22-l368 Updated error handler to map loader error response data to ErrorPage. Handled generic errors.

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,30 @@
-function ErrorPage(){
-  return (<p>An error occured!</p>)
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+import { useRouteError } from "react-router-dom";
+
+function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "something went wrong!";
+
+  if (error.statis === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title="An error occured!">
+        <p>Something went wrong!</p>
+      </PageContent>
+    </>
+  );
 }
 
 export default ErrorPage;
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -20,7 +20,10 @@ export async function loader() {
 
   if (!response.ok) {
     // return {isError: true, message: "Could not return events."}
-    throw { mesasge: "Could not fetch events." };
+    // throw { mesasge: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
   }
